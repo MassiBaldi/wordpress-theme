2e6c66bedcaaecbadd40b32d89a1196a380a41bc
MassiBaldi <massibaldi85@gmail.com>
2019-02-12 16:49:37 +0100

    navbar ok

--- functions.php
+++ functions.php
@@ -1,12 +1,25 @@
 <?php
 
-if (!function_exists('import_style')) {
+  if (!function_exists('import_style')) {
 
-  function import_style() {
-    wp_enqueue_style('massi', get_template_directory_uri() . '/style.css');
+    function import_style() {
+      wp_enqueue_style('massi', get_template_directory_uri() . '/style.css');
+    }
+
+    add_action('wp_enqueue_scripts', 'import_style');
   }
 
-  add_action('wp_enqueue_scripts', 'import_style');
-}
+  if (!function_exists('massi_setup')) {
+
+    function massi_setup() {
+
+      register_nav_menus(array(
+        'header' => 'header'
+      ));
+    }
+
+    add_action('after_setup_theme', 'massi_setup');
+  }
+
 
 ?>
